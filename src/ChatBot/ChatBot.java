1db6ed39e0253b820fed99e80e7c0fe707050c3c
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-11-25 17:18:48 +0200

    ChatBot {1-st} stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,2 +1,13 @@
-package ChatBot;public class ChatBot {
+package ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        String bot_name = "Sad-bot";
+        int birth_year = 2021;
+
+        // Приветствие
+        System.out.println("Hello! My name is " + bot_name
+                + "\nI was created in " + birth_year);
+        System.out.println("Please, remind me your name.");
+    }
 }
