10bf96256849d277d8988c28ff2f0789c5adfa40
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-11-25 17:21:36 +0200

    ChatBot {2-st} stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,7 +1,9 @@
 package ChatBot;
+import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         String bot_name = "Sad-bot";
         int birth_year = 2021;
 
@@ -9,5 +11,7 @@ public class ChatBot {
         System.out.println("Hello! My name is " + bot_name
                 + "\nI was created in " + birth_year);
         System.out.println("Please, remind me your name.");
+        String user_name = s.nextLine();
+        System.out.println("What a great name you have, " + user_name + '!');
     }
 }
