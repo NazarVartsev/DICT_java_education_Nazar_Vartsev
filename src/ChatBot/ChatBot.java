395f39fb233ab29a6d52435c391096e5442b945b
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-11-25 17:25:57 +0200

    ChatBot {4-st} stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -24,5 +24,12 @@ public class ChatBot {
         byte remainder7 = s.nextByte();
         int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+
+        // Счет
+        System.out.println("Now I will prove you that I can count to any number you want.");
+        int number_check = s.nextInt();
+        for (int i = 0; i <= number_check; i++) {
+            System.out.println(i + " !");
+        }
     }
 }
