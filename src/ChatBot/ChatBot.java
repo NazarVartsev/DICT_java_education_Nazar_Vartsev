c08b00ed074dd7360ff31b66ef513333e14b9cfb
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-11-25 17:15:10 +0200

    Revert "Назар@DESKTOP-LLVJDGL MINGW64 /d/JavaProg/GitProg (chatbot)"
    This reverts commit a6f9251ac336f2dc7fa653141a3001214ba881a3.

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,12 +1,2 @@
-package ChatBot;
-
-public class ChatBot {
-    public static void main(String[] args) {
-        String bot_name = "Sad-bot";
-        int birth_year = 2021;
-
-        // Приветствие
-        System.out.println("Hello! My name is " + bot_name
-                + "\nI was created in " + birth_year);
-    }
+package ChatBot;public class ChatBot {
 }
