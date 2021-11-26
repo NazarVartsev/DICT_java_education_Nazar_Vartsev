d4307da24c0a4e9cc343821ccb3ee798f177269a
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-11-26 04:00:09 +0200

    Class WS appdate #1

--- src/Hangman/WordsStorage.java
+++ src/Hangman/WordsStorage.java
@@ -3,24 +3,29 @@ package Hangman;
 import java.util.Random;
 
 public class WordsStorage {
+    private String word;
     private final String[] words_list = {"python", "java", "hot dog", "Mia Khalifa"};
 
     String hidingWords() {
-        String word;
+        String word_dash_out;
 
         Random r = new Random();
         int a_i = r.nextInt(3);
+        word = words_list[a_i];
+
         String word_dash = "";
         char dash = '-';
-        String word_r = words_list[a_i];
-        char[] wordChar = word_r.toCharArray();
-
-        int len_word_r = word_r.length() - 1;
+        char[] wordChar = word.toCharArray();
+        int len_word_r = word.length() - 1;
         int two_len_word_r = len_word_r - 2;
 
         for (int i = 0; i < two_len_word_r; i++) {
             word_dash += dash;
         }
-        return word = wordChar[0] + wordChar[1] + word_dash;
+        return word_dash_out = wordChar[0] + wordChar[1] + word_dash;
+    }
+
+    String getWord() {
+        return word;
     }
 }
